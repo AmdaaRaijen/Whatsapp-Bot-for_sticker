4e514c44ccee7e062ebf32610b16ff24e2048879
AmdaaRaijen <bintangtriadmaja@gmail.com>
2023-09-29 11:18:08 +0700

    feat: can create animated sticker by send video

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import { Client, LocalAuth, Message } from "whatsapp-web.js";
+import { Client, LocalAuth } from "whatsapp-web.js";
 import qrcode from "qrcode-terminal";
 
 const client = new Client({
   authStrategy: new LocalAuth(),
+  puppeteer: { args: ["--no-sandbox", "--disable-setuid-sandbox"] },
 });
 
 client.on("qr", (qr) => {
@@ -22,13 +23,21 @@ client.on("message", async (msg) => {
         const media = await msg.downloadMedia();
         client.sendMessage(msg.from, media, {
           sendMediaAsSticker: true,
-          stickerAuthor: "BOT-WA-STICKER/BT",
+          stickerAuthor: "sticker bot by @a.amdaa_",
           stickerName: "a.amdaa_",
         });
       } catch (err) {
         console.log(err);
         client.sendMessage(msg.from, "❌ Gagal membuat stiker");
       }
+    } else if (msg.hasMedia && msg.type == "video") {
+      client.sendMessage(msg.from, "⏳ Animated stiker sedang diproses...");
+      const media = await msg.downloadMedia();
+      client.sendMessage(msg.from, media, {
+        sendMediaAsSticker: true,
+        stickerAuthor: "sticker bot by @a.amdaa_",
+        stickerName: "a.amdaa_",
+      });
     } else {
       msg.reply(
         "*Bot Sedang Aktif*\n\nSilahkan kirim gambar yang akan dibuat stiker.\n\n*NOTE*\n\nSekarang anda *tidak perlu* menggunakan *caption* lagi untuk membuat stiker\n\nJaga kesopanan yaa... \nkirim gambar yang baik baik 🤨📸\n\nbot by @a.amdaa_"
